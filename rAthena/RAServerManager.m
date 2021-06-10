2c98c1ffc835421cf8a32e439f972c1c78eebc28
Leon Li <arkadeleon@gmail.com>
2021-06-10 17:14:58 +0800

    Upgrade sqlite database file after copying

--- rAthena/RAServerManager.m
+++ rAthena/RAServerManager.m
@@ -6,6 +6,7 @@
 //
 
 #import "RAServerManager.h"
+#import <sqlite3.h>
 
 @implementation RAServerManager
 
@@ -30,13 +31,71 @@
 
 - (void)copyBundleResourcesAndChangeDirectory {
     NSURL *src = [[NSBundle bundleForClass:[RAServerManager class]] bundleURL];
-    NSURL *dst = [[NSFileManager defaultManager] URLForDirectory:NSLibraryDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
-    NSArray<NSString *> *items = @[@"ragnarok.sqlite3", @"conf", @"db", @"npc"];
-    for (NSString *item in items) {
-        [[NSFileManager defaultManager] copyItemAtURL:[src URLByAppendingPathComponent:item] toURL:[dst URLByAppendingPathComponent:item] error:nil];
+    NSURL *dst = [[[NSFileManager defaultManager] URLForDirectory:NSLibraryDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil] URLByAppendingPathComponent:@"rathena"];
+
+    [[NSFileManager defaultManager] createDirectoryAtURL:dst withIntermediateDirectories:YES attributes:nil error:nil];
+
+    NSURL *srcDB = [src URLByAppendingPathComponent:@"ragnarok.sqlite3"];
+    NSURL *dstDB = [dst URLByAppendingPathComponent:@"ragnarok.sqlite3"];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:dstDB.path]) {
+        [[NSFileManager defaultManager] copyItemAtURL:srcDB toURL:dstDB error:nil];
     }
 
-    chdir(dst.path.UTF8String);
+    NSArray<NSString *> *paths = @[@"conf", @"db", @"npc"];
+    for (NSString *path in paths) {
+        NSURL *srcURL = [src URLByAppendingPathComponent:path];
+        NSURL *dstURL = [dst URLByAppendingPathComponent:path];
+        [[NSFileManager defaultManager] removeItemAtURL:dstURL error:nil];
+        [[NSFileManager defaultManager] copyItemAtURL:srcURL toURL:dstURL error:nil];
+    }
+
+    [[NSFileManager defaultManager] changeCurrentDirectoryPath:dst.path];
+
+    [self upgradeDatabaseAtURL:dstDB];
+}
+
+- (void)upgradeDatabaseAtURL:(NSURL *)url {
+    sqlite3 *db = nil;
+    sqlite3_open(url.path.UTF8String, &db);
+
+    NSURL *upgradeFilesDirectory = [[[NSBundle bundleForClass:[RAServerManager class]] bundleURL] URLByAppendingPathComponent:@"sql-files/upgrades"];
+    NSArray<NSURL *> *upgradeFiles = [[[NSFileManager defaultManager] contentsOfDirectoryAtURL:upgradeFilesDirectory includingPropertiesForKeys:nil options:0 error:nil] sortedArrayUsingComparator:^NSComparisonResult(NSURL *url1, NSURL *url2) {
+        return [url1.path compare:url2.path];
+    }];
+
+    for (NSURL *upgradeFile in upgradeFiles) {
+        NSString *upgradeID = upgradeFile.lastPathComponent.stringByDeletingPathExtension;
+
+        if (![upgradeID hasPrefix:@"upgrade_"]) {
+            continue;
+        }
+
+        NSString *sql = [NSString stringWithFormat:@"SELECT count(*) FROM upgrades WHERE id = '%@' LIMIT 1", upgradeID];
+        sqlite3_stmt *stmt = nil;
+        sqlite3_prepare_v2(db, sql.UTF8String, -1, &stmt, nil);
+        sqlite3_step(stmt);
+        int count = sqlite3_column_int(stmt, 0);
+        sqlite3_finalize(stmt);
+        stmt = nil;
+
+        if (count == 1) {
+            continue;
+        }
+
+        sql = [[NSString alloc] initWithContentsOfURL:upgradeFile encoding:NSUTF8StringEncoding error:nil];
+        sqlite3_prepare_v2(db, sql.UTF8String, -1, &stmt, nil);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
+        stmt = nil;
+
+        sql = [NSString stringWithFormat:@"INSERT INTO upgrades VALUES ('%@')", upgradeID];
+        sqlite3_prepare_v2(db, sql.UTF8String, -1, &stmt, nil);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
+        stmt = nil;
+    }
+
+    sqlite3_close(db);
 }
 
 @end
